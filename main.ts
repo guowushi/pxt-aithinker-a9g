51c4205a12d7a449ad0fb360ee9f0d45196cc270
guowushi <guowushi@qq.com>
2020-12-06 20:32:53 +0800

    delete no useed

--- main.ts
+++ main.ts
@@ -3,68 +3,24 @@
  */
 //% block="A9G" weight=100 color=#ff8f3f icon="\uf043"
 namespace A9G {
-    //network APN name, defined by user in initialization of network functions
+
+    enum LocationMode {
+        //% block="基站定位"
+        BaseRadioStaion=1,
+        //% block="GPS定位"
+        GPS=2
+    }
     let actualApnName = "internet";
-    //switches for debug purposes
     let echoEnabled = false; //should be alsways on false during normal operation
     let mqttSubscribeTopics: string[] = []; //订阅的主题
-
     let httpsConnected = false; //HTTP连接成功标志
     let requestFailed = false; //请求失败标志
-
     //定义经度和维度
     let Latitude = ""
     let Longitude = ""
 
 
-    /**
-     * 
-     */
-    function SetupHandlers() {
-        //attach listener
-        usbLogger.info(`Handlers init...`);
-        if (!echoEnabled) {  
-            serial.onDataReceived("+", function () {
-                basic.pause(50);
-                let dataRaw = serial.readString();
-                let data = dataRaw.substr(dataRaw.indexOf("+"), dataRaw.length);
-                //MQTT subscription received
-                if (data.includes("SMSUB:")) { 
-                    for (let i = 0; i < mqttSubscribeTopics.length; i++) {
-                        if (data.includes(mqttSubscribeTopics[i])) {
-                            let message = (data.split('","')[1]); // extract message from AT Response
-                            usbLogger.info(`MQTT subscription on topic: "${mqttSubscribeTopics[i]}" received content:"${message.slice(0, -3)}"`);
-                            mqttSubscribeHandler(mqttSubscribeTopics[i], message.slice(0, -3))
-                        }
-                    }
-                } else if (data.includes("CMTI:")) { //收到SMS
-                    let msgId = cloudConnectorUtils.trimString(data.split(",")[1]);
-                    let smsRaw = doSendAtCommand("AT+CMGR=" + msgId);
-                    let smsContent = cloudConnectorUtils.trimString(smsRaw.split("\n")[2]);
-                    let smsHeader = smsRaw.split("\n")[1];
-                    let senderPhoneNum = (smsHeader.split(","))[1];
-                    senderPhoneNum = senderPhoneNum.slice(1, senderPhoneNum.length - 1);
-                    usbLogger.info(`Received SMS with id: ${msgId}, message: ${smsContent}`);
-                    smsReceivedHandler(senderPhoneNum, smsContent);
-                    doSendAtCommand("AT+CMGD=0,1") // 删除已读短信
-                } else if (data.includes("SHREQ:")) {
-                    let dataSplit = data.split(",");
-                    let responseCode = dataSplit[1];
-                    let responseLength = dataSplit[2];
-                    usbLogger.info(`Got http response. Code: ${responseCode}, content length: ${responseLength}`);
-                    if (responseLength.includes("700")) { //this actually means error
-                        requestFailed = true;
-                        usbLogger.error(`Request failed`)
-                    } else if (responseLength.includes("680")) { //this is fine
-                        requestFailed = false
-                    }
-                } else if (data.includes("SHSTATE: 0")) {
-                    usbLogger.info(`Https connection broke`);
-                    httpsConnected = false
-                }
-            })
-        }
-    }
+    
     /**
      * 将整数转16进制
      * @param num 
@@ -89,38 +45,8 @@ namespace A9G {
         return result
     }
 
-    /**
-     * 确保GSM连接
-     */
-    function EnsureGsmConnection() {
-        let gsmStatus = GsmRegistrationStatus();
-        while (!(gsmStatus == 1 || gsmStatus == 5)) {
-            gsmStatus = GsmRegistrationStatus();
-            basic.pause(500);
-            usbLogger.info(`Waiting for GSM network. GSM status was ${gsmStatus}`)
-        }
-    }
+    
 
-
-    /**
-     * 确保GPRS连接(internal function)
-     */
-    function EnsureGprsConnection() {
-        doSendAtCommand('AT+CNACT=1,"' + actualApnName + '"');
-        basic.pause(1000);
-        let netStatus = doSendAtCommand('AT+CNACT?');
-        let tries = 0;
-        while (!netStatus.includes("+CNACT: 1")) {
-            if (tries >= 8) {
-                doSendAtCommand('AT+CNACT=1,"' + actualApnName + '"');
-                tries = 0
-            }
-            basic.pause(1000);
-            usbLogger.info(`Waiting for GPRS network connection`);
-            netStatus = doSendAtCommand('AT+CNACT?');
-            tries++
-        }
-    }
 
     /**
      * 发送AT命令 (internal function)
@@ -130,14 +56,15 @@ namespace A9G {
      * @param forceLogDisable  
      * @param additionalWaitTime 额外等待时间
      */
-    function doSendAtCommand(atCommand: string, timeout = 1000, useNewLine = true, forceLogDisable = false, additionalWaitTime = 0): string {
+     //% group="1. Setup: "
+    //% block="Send AT CMD", AT: $atCommand timeout: $timeout"
+    export function doSendAtCommand(atCommand: string, timeout = 1000, useNewLine = true, forceLogDisable = false, additionalWaitTime = 0): string {
         serial.readString(); //先清空buffer
         if (useNewLine) {
             serial.writeLine(atCommand)
         } else {
             serial.writeString(atCommand)
         }
-
         let startTs = input.runningTime();
         let buffer = "";
         // 读取串口直到超时
@@ -165,7 +92,9 @@ namespace A9G {
      * @param atCommand AT命令
      * @param limit 次数
      */
-    function SendAtCommandCheckAck(atCommand: string, limit = 5): boolean {
+     //% group="1. Setup: "
+    //% block="AT CMD and Check",AT: $atCommand retry times: $limit"
+    export function SendAtCommandCheckAck(atCommand: string, limit = 5): boolean {
         let tries = 0;
         let modemResponse = doSendAtCommand(atCommand, -1);
         while (!modemResponse.includes("OK")) {
@@ -184,6 +113,7 @@ namespace A9G {
      * @param tx 模块串口TX端对应的引脚 ,如 SerialPin.P2
      * @param rx 模块串口RX端对应的引脚 ，如SerialPin.P1
      */
+    //% group="1. Setup: "
     //% block="初始化A9G模块,TX: $tx RX: $rx"
     export function InitA9G(tx: SerialPin, rx: SerialPin) {
         /*
@@ -200,58 +130,25 @@ namespace A9G {
 
          while (!atResponse.includes("OK")) {
              atResponse = doSendAtCommand("AT", 1000);
-             usbLogger.info(`Trying to comunicate with modem...`)
+           //  usbLogger.info(`Trying to comunicate with modem...`)
          }
     }
 
-    function initLoggerIfNotInitialised() {
-        if (!usbLogger.initialised) {
-            usbLogger.init(SerialPin.P8, SerialPin.P16, BaudRate.BaudRate115200, usbLogger.LoggingLevel.INFO)
-        }
-    }
-    /**
-     * 
-     */
-    //% weight=100 blockId="A9G.init"
-    //% group="1. Setup: "
-    //% block="初始化A9G模块""
-    function init() {
-        initLoggerIfNotInitialised();
-        //发送AT指令，返回OK则说明正常
-        let atResponse = doSendAtCommand("AT");
-
-        while (!atResponse.includes("OK")) {
-            atResponse = doSendAtCommand("AT", 1000);
-            usbLogger.info(`Trying to comunicate with modem...`)
-        }
-        // doSendAtCommand("ATE " + (echoEnabled ? "1" : "0"));
-        //  doSendAtCommand("AT+CMEE=2"); // extend error logging
-        // doSendAtCommand("AT+CMGF=1"); // sms message text mode
-        //  doSendAtCommand("AT+CMGD=0,4"); // 删除所有SMS消息
-        // setupHandlers();
-        // forEuropeanVersionTurnOnlyCatM();
-        usbLogger.info(`Init done...`);
-    }
+    
 
     /**
      * 是否有SIM卡,返回
      * at+ccid   //查询ccid，确定是否有sim卡
      * +CCID: 898602A8221478DE0092
      */
+    //% group="1. Setup: "
     //% block="是否有SIM卡"
     export function HasSims(): boolean {
-        var serialRet = serial.writeLine("at+ccid")
+        let cmd ="at+ccid"
+        let atResponse = doSendAtCommand(cmd, 1000);
         return true
     }
-    /**
-     * at+creg?  //查询是否注册上网络
-        +CREG: 1,5 
-     */
-    //% block="附着GPRS网络"
-    export function IsOnline(): boolean {
-        serial.writeLine("at+creg?")
-        return true
-    }
+ 
     /**
      * 返回GSM网络注册状态, 1 或 5表示成功注册
      * 返回结果例子：+CREG: 1,5 
@@ -271,62 +168,45 @@ namespace A9G {
     /**
      * 附着网络
      */
+    //% group="6. GPS:"
     //% block="附着GPRS网络"
     export function RegGrpsNetwork() {
-        serial.writeLine("AT+CGATT=1")  //附着网络，如果需要上网，这条指令是必选的
-        serial.writeLine("AT+CGDCONT=1,\"IP\",\"CMNET\"")  // //设置PDP参数
-        serial.writeLine("AT+CGACT=1,1")        //激活PDP，正确激活以后就可以上网了
+        doSendAtCommand("AT+CGATT=1")  //附着网络，如果需要上网，这条指令是必选的
+        doSendAtCommand("AT+CGDCONT=1,\"IP\",\"CMNET\"")  // //设置PDP参数
+        doSendAtCommand("AT+CGACT=1,1")        //激活PDP，正确激活以后就可以上网了
     }
     /**
      * 开启GPS
      */
+     //% group="6. GPS: "
     //% block="开启GPS"
     export function GpsEnable() {
-        serial.writeLine("AT+GPS=1")  // 开启GPS 
-        serial.writeLine("AT+GPSRD=1")  // 查询GPS时间间隔为1秒
-        serial.writeLine("AT+GPSLP=1") // 设置GPS为低功耗模式
+        doSendAtCommand("AT+GPS=1")  // 开启GPS 
+        doSendAtCommand("AT+GPSRD=1")  // 查询GPS时间间隔为1秒
+        doSendAtCommand("AT+GPSLP=1") // 设置GPS为低功耗模式
+        doSendAtCommand("AT+GPSMD=2")  // 设置为GPS+BD 模式
+      //  usbLogger.info(`Gps Enabled...`)
     }
     /**
      * 查询地理位置,返回经度和纬度
+     *  @param mode 1：基站地址  2：GPS 地址
      */
+     //% group="6. GPS: "
     //% block="查询地理位置"
-    export function GetLocation() {
-        var ret = serial.writeLine("AT+LOCATION=1")
-        return ret
+    export function GetLocation(mode:LocationMode) {
+        let cmd = "AT+LOCATION=" + mode
+        let atResponse = doSendAtCommand(cmd)
+       // usbLogger.info(atResponse)
+        return atResponse
     }
-    /**
-     * 不停读取串口数据，解析GPS信息,直到返回经度和维度
-     */
-    //% block="解析GPS信息"
-    export function ReadGpsInfo() {
-        var _timeout = 60;
-        do {
-            let info = serial.readString()
-            if (info.indexOf('V,') != -1 && info.indexOf('V,') < 10 && parseFloat(Latitude) < 10) {
-                let location = info.split(',')
-                if (parseFloat(location[2]).toString() != 'NaN' && parseFloat(location[2]) > 1) {
-                    let index = (parseFloat(location[2]) / 100).toString().indexOf('.')
-                    Latitude = (parseFloat(location[2]) / 100).toString().slice(0, index + 5)
-
-                }
-            }
-            if (info.indexOf('N,') != -1 && info.indexOf('N,') < 10 && parseFloat(Longitude) < 10) {
-                let location = info.split(',')
-                if (parseFloat(location[2]).toString() != 'NaN' && parseFloat(location[2]) > 1) {
-                    let index = (parseFloat(location[2]) / 100).toString().indexOf('.')
-                    Longitude = (parseFloat(location[2]) / 100).toString().slice(0, index + 5)
-
-                }
-            }
-        } while (Latitude == "" || Longitude == "" || parseFloat(Longitude) < 10 || parseFloat(Latitude) < 10)
-        return [Latitude, Longitude];
-    }
+   
     /**
      * 发送TEXT短信
     
      * @param phone 接收者电话号码 +48333222111 "+(country code)(9-digit phone number)"
      * @param stext 短信内容
      */
+     //% group="3. GSM: "
     //% block="发送TEXT短信(只支持英文)"
     export function SendTextSms(phone: string, stext: string) {
         doSendAtCommand("AT+CMGF=1"); // set text mode
@@ -334,46 +214,13 @@ namespace A9G {
         doSendAtCommand(stext + "\x1A");
         usbLogger.info(`Sent SMS message`)
     }
-    /**
-     * 处理短信的回调
-     * @param handler 
-     */
-    //% weight=100 blockId="A9G.OnSmsReceived"
-    //% group="3. GSM: "
-    //% block="on SMS received from $senderNumber with $message"
-    //% draggableParameters
-    export function OnSmsReceived(handler: (senderNumber: string, message: string) => void) {
-        smsReceivedHandler = handler
-    }
-
-    /**
-     * 获取当前日期时间；格式为 "yy/MM/dd,hh:mm:ss±zz"
-     */
-    //% weight=100 blockId="A9G.DateAndTime"
-    //% group="3. GSM: "
-    //% block="date and time"
-    export function DateAndTime(): string {
-        doSendAtCommand("AT+CLTS=1"); // enable in case it's not enabled
-        let modemResponse = doSendAtCommand('AT+CCLK?');
-        if (modemResponse.includes('+CCLK:')) {
-            return modemResponse.split('"')[1]
-        }
-        return null
-    }
-    /**
-     * 发送PDU短信(支持中文)
-     * @param phone 接收者电话号码
-     * @param stext 短信内容
-     */
-    //% block="发送PDU短信(支持中文)"
-    export function SendPduSms(phone: string, stext: string) {
-
-    }
+   
     /**
      * 发送短信
      * @param phone 接收者电话号码
      * @param stext 短信内容
      */
+    //% group="3. GSM: "
     //% block="发送短信"
     export function SendSMS(phone: string, stext: string) {
         let ntext = ""
@@ -398,199 +245,7 @@ namespace A9G {
         serial.writeString(content)
         serial.writeBuffer(buffer)
     }
-    //--------------------------------------------------------------------------
-    /**
-     * 初始化MQTT
-     * @param apnName 
-     */
-    //% weight=100 blockId="cloudConnector.initMqtt"
-    //% group="4. MQTT:"
-    //% block="init MQTT with APN name: %apnName"
-    export function InitMqtt(apnName: string) {
-        actualApnName = apnName;
-        EnsureGsmConnection();
-        EnsureGprsConnection()
-    }
-    /**
-     * 连接MQTT服务器
-     * @param broker MQTT服务器地址
-     * @param topic  主题
-     */
-    //% block="连接MQTT服务器"
-    export function ConectMqtt(broker: string, port: number,clientId:string,userName:string,password:string) {
-        
-        SendAtCommandCheckAck(`AT+MQTTCONN="${broker}",${port},"${clientId}",120,0,"${userName}","${password}"`)
-
-    }
-
-    /**
-     * 发布消息
-     * @param topic 消息主题
-     * @param message 
-     * @param qos 
-     * @param retain 
-     */
-    //% weight=100 blockId="A9G.publishOnMqtt"
-    //% group="4. MQTT:"
-    //% block="publish on MQTT topic:%topic message:%message||qos:%qos retain:%retain"
-    //% qos.defl=1 retain.defl=0 expandableArgumentMode="toggle"
-    export function PublishOnMqtt(topic: string, message: string, qos = 1, retain = 0) :boolean{
-      
-        let cmd=`AT+MQTTPUB=${topic},${message},${qos},0,${retain}`
-        let modemResponse= doSendAtCommand(cmd, 100, true, true);
-        basic.pause(100);
-        if (modemResponse.includes("ERROR")) { 
-            return false
-        }
-        /*
-        let modemResponse = doSendAtCommand(message, 3000, false, true, 1000);
-
-        let tries = 0;
-        while ((modemResponse.includes("ERROR") || modemResponse.includes("SMSTATE: 0")) && (!(tries > 6))) {
-            usbLogger.info(`MQTT publish failed, retrying... attepmt: ${tries}`);
-            let modemNetState = doSendAtCommand("AT+CNACT?", -1);
-            let mqttConnectionState = doSendAtCommand("AT+SMSTATE?", -1);
-            if (modemNetState.includes("+CNACT: 0")) {
-                //network seem disconnected, try to reinit
-                InitMqtt(actualApnName);
-                SendAtCommandCheckAck("AT+SMCONN")
-            }
-            if (mqttConnectionState.includes("+SMSTATE: 0")) {
-                //seem like mqtt disconnection,try to reconnect
-                doSendAtCommand("AT+SMDISC");
-                SendAtCommandCheckAck("AT+SMCONN")
-            }
-            //retry message publishing
-            doSendAtCommand(cmd, 100);
-            modemResponse = doSendAtCommand(message, 5000, false, true);
-
-            tries++
-        }*/
-        usbLogger.info(`MQTT message on topic: "${topic}" published`)
-        return true
-    }
-
-    /**
-     * MQTT订阅(AT第二个参数为0表示取消订阅；第三个参数表示)
-     */
-    //% weight=100 blockId="A9G.SubscribeToMqtt"
-    //% group="4. MQTT:"
-    //% block="订阅MQT主题:%topic"
-    export function SubscribeToMqtt(topic: string,qos:number=0) {
-        let cmd = `AT+MQTTSUB="${topic}",1,${qos}`
-        doSendAtCommand(cmd, 100, true, true);
-        mqttSubscribeTopics.push(topic)
-    }
-
-
-    /**
-     * MQTT on subscription receive
-     */
-    //% weight=100 blockId="cloudConnector.onMqttMessageReceived"
-    //% group="4. MQTT:"
-    //% block="on MQTT $topic subscribtion with $message received"
-    //% draggableParameters
-    export function OnMqttMessageReceived(handler: (topic: string, message: string) => void) {
-        mqttSubscribeHandler = handler
-    }
-
-
-    /**
-     * MQTT Live Objects publish message
-     */
-    //% weight=100 blockId="A9G.PublishOnLiveObjects"
-    //% group="4. MQTT:"
-    //% block="publish data:%data with timestamp:%timestamp into Live Objects stream:%stream"
-    export function PublishIntoLiveObjects(data: string[], timestamp: string, stream: string) {
-        let dataString = '';
-        for (let i = 0; i < data.length; i++) {
-            dataString += ',"' + i + '":"' + data[i] + '"'
-        }
-
-        let liveObjectMsg = '{ "s":"' + stream + '", "v": { "timestamp":"' + timestamp + '"' + dataString + '} }';
-        PublishOnMqtt("dev/data", liveObjectMsg)
-    }
-
-    /**
-     * 默认接收短信处理
-     * @param fromNumber 
-     * @param message 
-     */
-    let smsReceivedHandler = function (fromNumber: string, message: string) {
-        usbLogger.warn(`Got SMS form ${fromNumber} but SMS received handler is not implemented!`
-            + `Skipping message ${message}`);
-    };
-    /**
-     * 默认MQTT订阅消息处理
-     * @param topic 
-     * @param message 
-     */
-    let mqttSubscribeHandler = function (topic: string, message: string) {
-        usbLogger.warn(`Subscribed for MQTT topic "${topic}" but MQTT subscribe handler is not implemented! `
-            + `Skipping message ${message}`);
-    };
-
-    //----------------------------------------------------------------
-    /**
-     * 初始化HTTP
-     * @param apnName 
-     */
-    //% weight=100 blockId="A9G.initHttp"
-    //% group="5. HTTP:"
-    //% block="init HTTP with APN name:%apnName"
-    export function InitHttp(apnName: string) {
-        actualApnName = apnName;
-        SendAtCommandCheckAck('AT+SAPBR=3,1,"APN","' + actualApnName + '"');
-        SendAtCommandCheckAck('AT+SAPBR=1,1');
-        SendAtCommandCheckAck('AT+SAPBR=2,1');
-        if (!SendAtCommandCheckAck('AT+HTTPINIT')) {
-            SendAtCommandCheckAck('AT+HTTPTERM');
-            SendAtCommandCheckAck('AT+HTTPINIT')
-        }
-    }
-
-    /**
-     * HTTP POST请求
-     * @param data POST请求数据
-     * @param url 请求地址
-     */
-    //% weight=100 blockId="A9G.httpPost"
-    //% group="5. HTTP:"
-    //% block="post data:%data through HTTP to url:%url"
-    export function httpPost(data: string, url: string) {
-        SendAtCommandCheckAck('AT+HTTPPARA="URL","' + url + '"');
-        doSendAtCommand("AT+HTTPDATA=" + data.length + ",1000");
-        basic.pause(100);
-        doSendAtCommand(data, 1000, false);
-        SendAtCommandCheckAck('AT+HTTPACTION=1')
-    }
-
-    /**
-  * GPS init
-  */
-    //% weight=100 blockId="A9G.gpsInit"
-    //% group="6. GPS:"
-    //% block="init GPS"
-    export function GpsInit() {
-        SendAtCommandCheckAck("AT+CGNSPWR=1")
-    }
-
-    /**
-     * 获取GPS定位
-     */
-    //% weight=100 blockId="A9G.getPosition"
-    //% group="6. GPS:"
-    //% block="获取GPS位置"
-    export function GpsPosition(): string {
-        let modemResponse = doSendAtCommand("AT+CGNSINF");
-        let position = "";
-        while (!modemResponse.includes("+CGNSINF: 1,1")) {
-            basic.pause(1000);
-            modemResponse = doSendAtCommand("AT+CGNSINF")
-        }
-        let tmp = modemResponse.split(",");
-        position = tmp[3] + "," + tmp[4];
-        return position
-    }
+     
+   
 
 }
